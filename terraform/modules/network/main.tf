d5ab67a4e96b94e61bda2b94941be72b61c48169
Sindre <sindre.demetrio@gmail.com>
2026-08-27 23:43:05 +0200

    feat(network): add router amd router nat

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,5 +1,7 @@
 locals {
   pod_secondary_range_name = "${var.subnet_name}-pods"
+  router_name              = "${var.network_name}-router"
+  nat_name                 = "${var.network_name}-nat"
 }
 
 resource "google_compute_network" "main" {
@@ -20,5 +22,30 @@ resource "google_compute_subnetwork" "main" {
   secondary_ip_range {
     range_name    = local.pod_secondary_range_name
     ip_cidr_range = var.pod_ipv4_cidr
+  }
+}
+
+resource "google_compute_router" "main" {
+  name    = local.router_name
+  project = var.project_id
+  region  = var.region
+  network = google_compute_network.main.id
+}
+
+resource "google_compute_router_nat" "main" {
+  name                               = local.nat_name
+  project                            = var.project_id
+  region                             = var.region
+  router                             = google_compute_router.main.name
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.main.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
   }
 }
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -22,3 +22,13 @@ output "pod_secondary_range_name" {
   description = "Name of the subnet secondary range allocated to Pods."
   value       = local.pod_secondary_range_name
 }
+
+output "router_name" {
+  description = "Name of the Cloud Router used by Cloud NAT."
+  value       = google_compute_router.main.name
+}
+
+output "nat_name" {
+  description = "Name of the Cloud NAT configuration."
+  value       = google_compute_router_nat.main.name
+}

00aa09499c254664fa2d10fe2fe35dcc78738606
Sindre <sindre.demetrio@gmail.com>
2026-08-27 20:25:58 +0200

    feat: add terraform foundation

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z][a-z0-9-]{4,28}[a-z0-9]$", var.project_id))
+    error_message = "The project_id must be a valid GCP project ID"
+  }
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "europe-north1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "europe-north1-a"
+}
